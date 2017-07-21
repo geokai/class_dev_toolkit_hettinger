aec53d58703376e73295d1835ee249813e280a5a
George Kaimakis <geomatus@mac.com>
2017-07-21 19:33:33 +0100

    Adds first client api - acadamia.py:

---
+++ classes_toolkit/academia.py
@@ -0,0 +1,19 @@
+"""Implementing Circuituous (tm) 'area' method on a range of circles"""
+
+from circuituous import Circle
+from random import random, seed
+
+
+print()
+print('Using Circuituous (tm) version', Circle.version)
+print()
+
+seed(8675309)
+n = 10
+circles = [Circle(random()) for i in range(n)]
+
+print('The average area of', n, 'random circles ', end='')
+avg = sum([c.area() for c in circles]) / n
+print('is {0:0.3f}'.format(avg))
+
+print()
